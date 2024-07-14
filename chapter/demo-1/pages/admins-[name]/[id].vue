1baf5316257454e90cd55312cb9129a742f6a23b
weijingbbb <1047129443@qq.com>
2024-07-14 18:58:26 +0800

    chapter-3 35 路由传参 localhost:3000/admins-bbb/19 半静态路由

---
+++ chapter/demo-1/pages/admins-[name]/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>admin-name: {{ adminname }}</h1>
+        <h1>admin-id: {{ adminid }}</h1>
+    </div>
+</template>
+<script setup>
+const route = useRoute()
+const adminname = route.params.name
+const adminid = route.params.id
+</script>
