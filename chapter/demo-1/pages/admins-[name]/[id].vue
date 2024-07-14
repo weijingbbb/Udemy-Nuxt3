2fb4fce4d0b7ec03aee28b332a789458b7985670
weijingbbb <1047129443@qq.com>
2024-07-14 22:41:42 +0800

    chapter-3 44 路由校验 validate ，但更多使用的是middleware

--- chapter/demo-1/pages/admins-[name]/[id].vue
+++ chapter/demo-1/pages/admins-[name]/[id].vue
@@ -5,6 +5,12 @@
     </div>
 </template>
 <script setup>
+definePageMeta({
+    validate: (route) => {
+        // 判断是否是数字
+        return /^\d+$/.test(route.params.id)
+    }
+})
 const route = useRoute()
 const adminname = route.params.name
 const adminid = route.params.id
