066205b1645209be9b61267fbdc138fd0a4cf0e3
weijingbbb <1047129443@qq.com>
2024-07-14 18:54:43 +0800

    chapter-3 35 路由传参http://localhost:3000/user/bbb/18

---
+++ chapter/demo-1/pages/user/[name]/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>name: {{ name }}</h1>
+        <h1>id: {{ id }}</h1>
+    </div>
+</template>
+<script setup>
+const route = useRoute()
+const name = route.params.name
+const id = route.params.id
+</script>
