d72c7fbde48a21a97a83f1e30e01604c83936ac1
weijingbbb <1047129443@qq.com>
2024-07-14 16:55:22 +0800

    chapter-3 初始化demo

---
+++ chapter/demo-1/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts里的配置就是对Nuxt应用的扩展
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
