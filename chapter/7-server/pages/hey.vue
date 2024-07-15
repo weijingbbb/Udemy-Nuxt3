f39fb8527ee45d5db451bd483d5dd817bd370149
weijingbbb <1047129443@qq.com>
2024-07-16 00:40:07 +0800

    chapter-7 87 server 路由的方式添加接口，但不能和页面路径相同，否则是调用接口，而不是访问页面

---
+++ chapter/7-server/pages/hey.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>hey页</h1>
+        {{ data }}
+    </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/hey')
+
+
+</script>
+
+<style scoped lang="scss"></style>
---
+++ chapter/7-server/server/routes/hey.js
@@ -0,0 +1,10 @@
+export default defineCachedEventHandler(
+    (event) => {
+        // return '???bbb'
+        return [
+            'aaa',
+            'bbb',
+            'ccc'
+        ]
+    }
+)
