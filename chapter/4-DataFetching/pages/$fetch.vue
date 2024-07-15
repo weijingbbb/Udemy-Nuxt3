d4941303bddb5ec907a8acc76c670f899fd7e354
weijingbbb <1047129443@qq.com>
2024-07-15 18:06:48 +0800

    chapter-4 74 useFetch  $fetch 会在服务端和客户端各产生一次请求，好像是在客户端水合之后进行请求

---
+++ chapter/4-DataFetching/pages/$fetch.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ toDo }}
+        $fetch测试页
+    </div>
+</template>
+
+<script setup>
+const toDo = await $fetch('https://jsonplaceholder.typicode.com/todos/1')
+console.log('toDo---', toDo);
+
+</script>
+
+<style scoped lang="scss"></style>
