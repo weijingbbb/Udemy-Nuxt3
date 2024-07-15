d40f352f124f28c308caf88a5669df2ec29ee019
weijingbbb <1047129443@qq.com>
2024-07-15 19:35:28 +0800

    chapter-5 79 useState 使用 shallowRef 浅引用优化渲染

---
+++ chapter/5-useState/pages/rank.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const rank = useState('rank', () => {
+        return shallowRef({
+            num: 3
+        })
+    })
+    const chang = () => {
+        rank.value.num = 7
+    }
+</script>
+
+<template>
+    <div>
+        <div>目前排名是: {{ rank }}</div>
+        <button @click="chang">点击上升排名</button>
+    </div>
+</template>
