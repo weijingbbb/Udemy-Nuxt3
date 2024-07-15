ba0018716bac18db47704ec528feb7f41302f46d
weijingbbb <1047129443@qq.com>
2024-07-15 13:45:37 +0800

    chapter-4 57 useFetch 发送post请求

---
+++ chapter/4-DataFetching/pages/fetching-post.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>这个是fetching-post页<div>{{ posts }}</div></div>
+</template>
+
+<script setup>
+const { data: posts } = await useFetch('posts', {
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    method: 'post',
+    body: {
+        title: '哈啰',
+        body: '你好呀',
+        userID: 99
+    }
+})
+console.log('posts---: ', posts.value);
+</script>
