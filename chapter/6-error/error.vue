0b3e767614f26f94ec5969ef15cccb01c51bc2a7
weijingbbb <1047129443@qq.com>
2024-07-15 19:55:50 +0800

    chapter-6 83 error 接收错误信息、全局错误页面

---
+++ chapter/6-error/error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>错误代码：{{ error.statusCode }}</h1>
+        <h1>错误信息：{{ error.message }}</h1>
+        <p>{{ error }}</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+    console.log('error-----', error);
+</script>
