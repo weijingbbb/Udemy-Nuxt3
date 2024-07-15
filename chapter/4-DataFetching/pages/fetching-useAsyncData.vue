890cfdda17ecfd068cd6e10cbf3f75415267ef35
weijingbbb <1047129443@qq.com>
2024-07-15 18:49:17 +0800

    chapter-4 75 useAsyncData的使用方式，  useFetch就是useAsyncData + $fetch 的封装，当一个页面多个请求时，还是使用useAsyncData + $fetch吧

---
+++ chapter/4-DataFetching/pages/fetching-useAsyncData.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>这个是useAsyncData页面</div>
+</template>
+
+<script setup>
+    const { data } = await useAsyncData('product', async () => {
+        const [products, categories, todos] = await Promise.all([
+            $fetch('https://fakestoreapi.com/products'),
+            $fetch('https://fakestoreapi.com/products/categories'),
+            $fetch('https://jsonplaceholder.typicode.com/todos'),
+        ])
+        return { products, categories, todos  }
+    })
+    console.log(data.value);
+    console.log(data.value.products);
+    console.log(data.value.categories);
+    console.log(data.value.todos);
+</script>
