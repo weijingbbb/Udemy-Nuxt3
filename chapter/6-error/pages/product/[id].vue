1f90b28da5437167d7bca4ba71b2f36134cc8230
weijingbbb <1047129443@qq.com>
2024-07-15 20:09:54 +0800

    chapter-6 84 error 主动抛出错误

---
+++ chapter/6-error/pages/product/[id].vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ product }}
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const id = route.params.id
+const { data: product, error } = await useFetch(`https://fakestoreapi.com/products/${id}`)
+
+if(!product.value) {
+    throw createError({
+        statusCode: 404,
+        message: '没有找到这个页面'
+    })
+}
+</script>
+
+<style scoped lang="scss"></style>
