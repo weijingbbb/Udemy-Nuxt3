f4fadee6bc7ff45a751525facaceb3e67a09faa9
weijingbbb <1047129443@qq.com>
2024-07-15 13:52:30 +0800

    chapter-4 58 useFetch 发送patch请求

---
+++ chapter/4-DataFetching/pages/fetching-patch.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>这个是fetching-patch页<div>{{ posts }}</div></div>
+</template>
+
+<script setup>
+const { data: posts } = await useFetch('posts/1', {
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    method: 'patch',
+    body: {
+        title: '打补丁',
+    }
+})
+console.log('posts---: ', posts.value);
+</script>
