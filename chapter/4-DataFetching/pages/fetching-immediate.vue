cadb5c9d2cd9f05ac9d31bb81032877f50d39551
weijingbbb <1047129443@qq.com>
2024-07-15 15:02:38 +0800

    chapter-4 64 useFetch default 设置默认数据

--- chapter/4-DataFetching/pages/fetching-immediate.vue
+++ chapter/4-DataFetching/pages/fetching-immediate.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
         <div>这个是fetching-immediate</div>
+        <div>这里渲染结果：{{ posts }}</div>
         <button @click="execute">请求数据呀</button>
     </div>
 </template>
 
 <script setup>
 // immediate，默认初始化时不调用请求，把执行的权力交给用户调用
+// default设置默认数据
 const { data: posts, execute } = await useFetch('products/1', {
     baseURL: 'https://fakestoreapi.com',
     immediate: false,
+    default: (product) => {
+        product = '没有数据呀，点击进行请求呀！'
+        return product
+    }
 })
 console.log('posts---: ', posts.value);
 </script>
