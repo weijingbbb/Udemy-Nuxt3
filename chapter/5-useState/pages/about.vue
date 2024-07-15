545da5938d88aee278ae2f105fb092dcdeb68714
weijingbbb <1047129443@qq.com>
2024-07-15 19:18:32 +0800

    chapter-5 76 useState 初始化demo页面，各种页面使用自己的state

---
+++ chapter/5-useState/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>这个是about页</h1>
+        <div>当前的值是：<span style="font-size: 38px; color: red;">{{ counter }}</span></div>
+        <button @click="add">点击值+1</button>
+        <div>
+            <NuxtLink to="/">返回首页</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const counter = useState(() => 1)
+const add = () => {
+    counter.value = counter.value + 1
+}
+</script>
