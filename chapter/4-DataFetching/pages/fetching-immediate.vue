980ccb63bd3b3c276294ba73cba57edb821a93c0
weijingbbb <1047129443@qq.com>
2024-07-15 14:58:16 +0800

    chapter-4 63 useFetch immediate参数，初始化时不调用请求获取数据，交给用户处理请求

---
+++ chapter/4-DataFetching/pages/fetching-immediate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>这个是fetching-immediate</div>
+        <button @click="execute">请求数据呀</button>
+    </div>
+</template>
+
+<script setup>
+// immediate，默认初始化时不调用请求，把执行的权力交给用户调用
+const { data: posts, execute } = await useFetch('products/1', {
+    baseURL: 'https://fakestoreapi.com',
+    immediate: false,
+})
+console.log('posts---: ', posts.value);
+</script>
