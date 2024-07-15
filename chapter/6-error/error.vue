984a4b2fd11b0aa6f5f8538d84bd640d2bec0d13
weijingbbb <1047129443@qq.com>
2024-07-15 20:16:06 +0800

    chapter-6 85 error 清除错误，重定向到别的页面

--- chapter/6-error/error.vue
+++ chapter/6-error/error.vue
@@ -3,10 +3,16 @@
         <h1>错误代码：{{ error.statusCode }}</h1>
         <h1>错误信息：{{ error.message }}</h1>
         <p>{{ error }}</p>
+        <button @click="handleError">清除错误，返回产品页</button>
     </div>
 </template>
 
 <script setup>
     defineProps(['error'])
-    console.log('error-----', error);
+    // console.log('error-----', error);
+    const handleError = () => {
+        clearError({
+            redirect: '/product/1'
+        })
+    }
 </script>
