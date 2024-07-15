ae57f3f52c2bd8a0f99e8ac1d701add47e37463c
weijingbbb <1047129443@qq.com>
2024-07-15 15:07:43 +0800

    chapter-4 65 useFetch transform 可以对得到的请求结果进行转换、过滤

--- chapter/4-DataFetching/pages/fetching-immediate.vue
+++ chapter/4-DataFetching/pages/fetching-immediate.vue
@@ -9,12 +9,19 @@
 <script setup>
 // immediate，默认初始化时不调用请求，把执行的权力交给用户调用
 // default设置默认数据
+// 进行参数转换
 const { data: posts, execute } = await useFetch('products/1', {
     baseURL: 'https://fakestoreapi.com',
     immediate: false,
     default: (product) => {
         product = '没有数据呀，点击进行请求呀！'
         return product
+    },
+    transform: (product) => {
+        return {
+            ...product,
+            title: '转换后的标题呀'
+        }
     }
 })
 console.log('posts---: ', posts.value);
