e65b3863016c96fd9a477373b634733455c30083
weijingbbb <1047129443@qq.com>
2024-07-15 19:32:18 +0800

    chapter-5 78 useState 使用composeables封装共享数据

--- chapter/5-useState/pages/compose2.vue
+++ chapter/5-useState/pages/compose2.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <h1>这个是首页</h1>
+        <h1>这个是compose2页</h1>
         <div>当前的值是：<span style="font-size: 38px; color: red;">{{ counter }}</span></div>
         <button @click="add">点击值+1</button>
         <div>
-            <NuxtLink to="/about">去about页</NuxtLink>
+            <NuxtLink to="/">去首页</NuxtLink>
         </div>
         <div>
             <NuxtLink to="/compose">去compose页</NuxtLink>
@@ -13,7 +13,7 @@
 </template>
 
 <script setup>
-const counter = useState('counter',() => 1)
+const counter = useCounter()
 const add = () => {
     counter.value = counter.value + 1
 }
