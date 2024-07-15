7647c7aaa56d8ceaeb67f0331cfb3eea52aeb4cc
weijingbbb <1047129443@qq.com>
2024-07-15 13:48:29 +0800

    chapter-4 58 useFetch 发送put请求

---
+++ chapter/4-DataFetching/pages/fetching-put.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>这个是fetching-put页<div>{{ put }}</div></div>
+</template>
+
+<script setup>
+const { data: put } = await useFetch('posts/1', {
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    method: 'put',
+    body: {
+        title: '更新数据',
+        body: '快点呀',
+        userID: 99
+    }
+})
+console.log('put---: ', put.value);
+</script>
